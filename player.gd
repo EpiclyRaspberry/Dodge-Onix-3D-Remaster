5e2c6906fb6bf6bd296213b5da6cf236908cab72
namooTH <73419369+namooTH@users.noreply.github.com>
2023-09-21 18:45:16 +0700

    fire

--- player.gd
+++ player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 5
 const JUMP_VELOCITY = 4.5
+
+var ACCERATION = 0
 
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -14,40 +16,36 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			neck.rotate_y(-event.relative.x * 0.01)
-			camera.rotate_x(-event.relative.y * 0.01)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
-
-func _ready():
-	get_tree().paused = false
-
-
+			camera.rotate_x(-event.relative.y * 0.01) 
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+			
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if Input.is_action_just_released("ui_cancel"):
-		get_tree().paused = true
-
 	if Input.is_action_pressed("crouch"):
 		$CollisionShape3D.scale.y = 0.5
 	else:
 		$CollisionShape3D.scale.y = 1
-
-
+		
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		print(direction)
+		ACCERATION += 0.1
+		print(direction)
+		velocity.x = direction.x * SPEED * ACCERATION
+		velocity.z = direction.z * SPEED * ACCERATION
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = ACCERATION * SPEED
+		velocity.z = ACCERATION * SPEED
+
+	print(velocity)
 
 	move_and_slide()
